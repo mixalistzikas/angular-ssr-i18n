9bca7f03e9600ea5d1843d60254b1f6c2bd54d61
mixalistzikas <mixalistzikas@gmail.com>
2018-07-09 08:41:29 +0300

    first commit

---
+++ scripts/build.js
@@ -0,0 +1,25 @@
+const execSync = require('child_process').execSync;
+const path = require('path');
+const project = require(path.join(process.cwd(), 'project.json'));
+const args = require('yargs').argv;
+
+const locales = Object.keys(project.locales);
+
+const baseCommand = args.server === 'true' ? project.server.command : project.client.command;
+const folder = args.server === 'true' ? project.server.output : project.client.output;
+
+function buildCommand(command, locale, output, needFile) {
+  command += ` --output-path dist/${output}/${locale.toLowerCase()} --deploy-url /${locale.toLowerCase()}/ --base-href /${locale.toLowerCase()}/ --i18n-locale=${locale}`;
+  if (needFile) {
+    command += ` --i18n-file=src/i18n/messages.${locale.toLowerCase()}.xlf --i18n-format=xlf`;
+  }
+  return command;
+}
+
+for (let i = 0; i < locales.length; i++) {
+  console.info(`Building for locale ${locales[i]}...`);
+  execSync(buildCommand(baseCommand, locales[i], folder, project.locales[locales[i]]), { cwd: process.cwd(), stdio: [0, 1, 2] });
+  console.info();
+}
+
+process.exit(0);
